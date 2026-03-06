183c54bed6d496a93bd77475fd7fee279aa978f7
BayuAlif <bayualif170505@gmail.com>
2026-03-06 18:10:10 +0700

    Pembuatan logic bankService

--- BankService.java
+++ BankService.java
@@ -9,5 +9,33 @@ package com.mycompany.encapsulation_pbo;
  * @author bayu
  */
 public class BankService {
-    
+     public void deposit(BankAccount account, int amount) {
+
+        if(amount > 0) {
+            account.setBalance((int) amount);
+            System.out.println("Deposit berhasil: " + amount);
+        } else {
+            System.out.println("Jumlah deposit tidak valid");
+        }
+
+    }
+
+    public void withdraw(BankAccount account, int amount) {
+
+        if(amount <= account.getBalance()) { // validation 2
+            account.reduceBalance((int) amount);
+            System.out.println("Withdraw berhasil: " + amount);
+        } else {
+            System.out.println("Saldo tidak cukup");
+        }
+
+    }
+
+    public void checkBalance(BankAccount account) {
+
+        System.out.println("Nama: " + account.getName());
+        System.out.println("No Rekening: " + account.getAccountNumber());
+        System.out.println("Saldo: " + account.getBalance());
+
+    }
 }
